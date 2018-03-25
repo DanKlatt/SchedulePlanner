25cae276014f4f54f149d147fcb012a709459baa
Daniel Klatt <dk0034@uah.edu>
2018-03-25 15:20:48 -0500

    Initial Commit
    Basic application class and gitignore file

---
+++ src/edu/uah/cs321/team2/scheduleplanner/SchedulePlanner.java
@@ -0,0 +1,32 @@
+package edu.uah.cs321.team2.scheduleplanner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Team 2
+ */
+public class SchedulePlanner extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
