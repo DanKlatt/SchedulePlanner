e94ab30bc939007884e698c5f9fd19026f820697
Daniel Klatt <dk0034@uah.edu>
2018-03-28 20:47:15 -0500

    First pass at CompositeSchedule
    Basic CompositeSchedule class with basically everything commented out
    until other models are in place

---
+++ src/edu/uah/cs321/team2/scheduleplanner/model/CompositeSchedule.java
@@ -0,0 +1,70 @@
+package edu.uah.cs321.team2.scheduleplanner.model;
+
+import java.util.ArrayList;
+
+/**
+ * The model that contains the collections of data that make up
+ * a composite schedule
+ * @author Team 2
+ */
+public class CompositeSchedule {
+    /**
+     * Constructs a CompositeSchedule object with empty lists
+     */
+    public CompositeSchedule() {
+        /*
+        this.people = new ArrayList<Person>();
+        this.shifts = new ArrayList<Shift>();
+        */
+    }
+    
+    /**
+     * Retrieves a copy of the people list
+     * 
+     * @return A copy of people
+     */
+    /*
+    public ArrayList<Person> getPeople() {
+        return new ArrayList<Person>(this.people);
+    }
+    */
+    
+    /**
+     * Retrieves a copy of the shifts list
+     * 
+     * @return A copy of shifts
+     */
+    /*
+    public ArrayList<Shift> getShifts() {
+        return new ArrayList<Shift>(this.shifts);
+    }
+    */
+    
+    /**
+     * Adds a person to the collection of available people
+     * 
+     * @param newPerson The new person who will be added to the list of people
+     */
+    /*
+    public void addPersonToPeople(Person newPerson) {
+        this.people.add(newPerson);
+        // send out a notification?
+    }
+    */
+    
+    /**
+     * Adds a shift to the collection of available shifts
+     * 
+     * @param newShift The new shift that will be added to the list of shifts
+     */
+    /*
+    public void addShiftToShifts(Shift newShift) {
+        this.shifts.add(newShift);
+        // send out a notification?
+    }
+    */
+    
+    // These fields will have to wait until the other models are in place...
+    // private ArrayList<Person> people;
+    // private ArrayList<Shift> shifts;
+}
