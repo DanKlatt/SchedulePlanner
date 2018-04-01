7bc0ce133d388920b9b2316b542f6d5993a77aa8
melissamartin180 <37888941+melissamartin180@users.noreply.github.com>
2018-04-01 14:08:36 -0500

    Add Person class to model

---
+++ src/edu/uah/cs321/team2/scheduleplanner/model/Person.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Melissa
+ */
+public class Person {
+
+    public Person(String name, String phone, Role role){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    public String getRole(){
+        //return roleName;
+    }
+    public void setRole(Role role){
+        this.role = role;
+    }
+    
+    
+    
+    
+    
+    private String name;
+    private String phone;
+    private Role role;
+    
+    
+}
