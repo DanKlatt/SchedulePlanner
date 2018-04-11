3cc2c2e984d7f8781223e2b35075a9ea1ceeeddc
melissamartin180 <37888941+melissamartin180@users.noreply.github.com>
2018-04-10 23:14:33 -0500

    Edited Person (model, view, and controller)

--- src/edu/uah/cs321/team2/scheduleplanner/view/AddEditPersonController.java
+++ src/edu/uah/cs321/team2/scheduleplanner/view/AddEditPersonController.java
@@ -8,15 +8,34 @@ package edu.uah.cs321.team2.scheduleplanner.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
-//import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.*;
-
+import edu.uah.cs321.team2.scheduleplanner.model.Person;
+import edu.uah.cs321.team2.scheduleplanner.model.Role;
+import edu.uah.cs321.team2.scheduleplanner.PersonDelegate;
+//import javafx.collections.ObservableList;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Melissa
  */
 public class AddEditPersonController {
+    
+    private PersonDelegate delegate;
+    
+    //Person object to be set by setPerson function if controller is used for editing a person
+    private Person aPerson = null;
+    
+    private String fName;
+    private String lName;
+    private String phone;
+    private String roleName;
+    private Role role;
+    private RadioButton selectedRadioButton;
+    
+    //set note above selectedRadioButton in handleSaveButtonAction
+    //private boolean roleSelectedStatus;
+         
     
     //text field for the first name
     @FXML
@@ -31,31 +50,82 @@ public class AddEditPersonController {
     private TextField phoneNum;
     
     @FXML
+    private ToggleGroup roles;
+    
+    @FXML
     private Button saveButton;
     
     @FXML
     private Button cancelButton;
     
     @FXML
-    public void initialize() {
-        //not sure what to put here
-    }
+    private RadioButton Worker;
     
+    @FXML
+    private RadioButton Manager;
+    
+       
+    @FXML
+    public void initialize() {
+        //the case that a person was 
+        if(aPerson != null){
+            firstName.setText(aPerson.getFirstName());
+            lastName.setText(aPerson.getLastName());
+            phoneNum.setText(aPerson.getPhone());
+            roleName = aPerson.getRole();
+            if(roleName.equals("Worker")){
+                Worker.setSelected(true);
+            }
+            else{
+                Manager.setSelected(true);
+            }
+        }
+    }
+    public void setPerson(Person person){
+        this.aPerson = person;
+    }
     //function to handle when the save button is pressed
     @FXML
-    private void handleSaveButtonAction(ActionEvent event) {
+    public void handleSaveButtonAction(ActionEvent event) {
         
-        String fName = firstName.getText().trim();
-        String lName = lastName.getText().trim();
-        String phone = phoneNum.getText().trim();
+        fName = firstName.getText().trim();
+        lName = lastName.getText().trim();
+        phone = phoneNum.getText().trim();
         
-        //handles the case that any of the fields are empty
-        if( fName.isEmpty() || lName.isEmpty() || phone.isEmpty()) {
+        //I hoping if no radio button is selected then in returns null
+        //otherwise I will need to use function isRadioButtonSelected I created
+        selectedRadioButton = (RadioButton)roles.getSelectedToggle();
+        
+        //handles the case that any of the fields are empty -- see note above about selectedRadioButton, it may error
+        if (fName.isEmpty() || lName.isEmpty() || phone.isEmpty() || selectedRadioButton == null) {
             Alert alert = new Alert(AlertType.ERROR, "All fields require an entry.");
             alert.show();
         }
-        else {
-            //send the data out
+        //case that all field had values
+        else {            
+            //get the role from the selected radio button
+            roleName = selectedRadioButton.getText();
+            
+            if(roleName.equals("Worker")){
+                role = new Role(0);
+            }
+            else{
+                role = new Role(1);
+            }
+            //check if aPerson is null and therefore in add mode
+            if(aPerson==null){ 
+                //send the data out
+                aPerson = new Person(fName, lName, phone, role);
+                delegate.addPerson(aPerson);
+            }
+            //case that the aPerson was set and therefore in edit mode
+            else{
+                aPerson.setFirstName(fName);
+                aPerson.setLastName(lName);
+                aPerson.setPhone(phone);
+                aPerson.setRole(role);
+            }
+            
         }
     }
     
@@ -63,10 +133,25 @@ public class AddEditPersonController {
     @FXML
     private void handleCancelButtonAction(ActionEvent event){
     //make this action act like pressing the x button
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
     }
-       
+
     
-    //scheduleData.getInstance().addPerson(new Person(fName,lName, phone, role_type));
-   
-    
+    /*function used to check if a radio button is selected. I can't find
+    a way for java to check if a no selection is made so I just wrote my 
+    own function. Hopefully a toggleGroup.getselectedToggle returns null, then
+    I won't have to use this function
+    private boolean isRadioButtonSelected (ToggleGroup T){
+        boolean selected = false;
+        //loop thru all the radio buttons that belong to the given toggle group
+        //and see if they are selected
+        for(Toggle t : T.getToggles()){
+            //will set selected to true if any button is selected 
+            if(selected==false){
+                selected = t.isSelected();
+            }
+        }
+        return selected;
+        
+    } */    
 }
