41378d247e2ae062630b0d80d5f3c0b1b90151ac
Sameera <sfw0007@uah.edu>
2018-04-11 20:51:36 -0500

    modified people list controller

--- src/edu/uah/cs321/team2/scheduleplanner/view/PeopleListViewController.java
+++ src/edu/uah/cs321/team2/scheduleplanner/view/PeopleListViewController.java
@@ -26,41 +26,41 @@ public class PeopleListViewController {
     
     @FXML
     //method to fill dummy data of people's information
-    public void initialize {
-    Role Manager = new Role(Type_Manager);
-    Role Worker = new Role(Type_Worker);
-    Person person1 = new Person("James Smith", "256-479-3444", Manager);
-    Person person2 = new Person("Martin Kane", "256-352-9765", Manager);
-    Person person3 = new Person("Leah Bowman", "256-541-4760", Worker);
-    Person person4 = new Person("Jim Walter", "256-772-1005", Worker);
-    Person person5 = new Person("Felicity Jones", "256-912-3725", Worker);
-    Person person6 = new Person("Sammy Tucker", "256-998-2263", Worker);
-    Person person7 = new Person("Alison Redding", "256-923-4567", Worker);
-    Person person8 = new Person("James Smith", "256-321-2140", Worker);
-    Person person9 = new Person("Zack Hancock", "256-555-0000", Worker);
-    Person person10 = new Person("Shay Lim", "256-655-0130", Worker);
+    //public void initialize {
+    //Role Manager = new Role(Type_Manager);
+    //Role Worker = new Role(Type_Worker);
+    //Person person1 = new Person("James Smith", "256-479-3444", Manager);
+    //Person person2 = new Person("Martin Kane", "256-352-9765", Manager);
+    //Person person3 = new Person("Leah Bowman", "256-541-4760", Worker);
+    //Person person4 = new Person("Jim Walter", "256-772-1005", Worker);
+    //Person person5 = new Person("Felicity Jones", "256-912-3725", Worker);
+    //Person person6 = new Person("Sammy Tucker", "256-998-2263", Worker);
+    //Person person7 = new Person("Alison Redding", "256-923-4567", Worker);
+    //Person person8 = new Person("James Smith", "256-321-2140", Worker);
+    //Person person9 = new Person("Zack Hancock", "256-555-0000", Worker);
+    //Person person10 = new Person("Shay Lim", "256-655-0130", Worker);
     
-    People = new ArrayList<Person>();
-    People.add(person1);
-    People.add(person2);
-    People.add(person3);
-    People.add(person4);
-    People.add(person5);
-    People.add(person6);
-    People.add(person7);
-    People.add(person8);
-    People.add(person9);
-    People.add(person10);
+    //People = new ArrayList<Person>();
+    //People.add(person1);
+    //People.add(person2);
+    //People.add(person3);
+    //People.add(person4);
+    //People.add(person5);
+    //People.add(person6);
+    //People.add(person7);
+    //People.add(person8);
+    //People.add(person9);
+    //People.add(person10);
     
-}  
+//}  
     public void EditPerson() {
- 
+        
 }
     public void AddPerson() {
     
 }
     public void GettingScene() {
-        Scene scene = <rootNode>.getScene();
+    //    Scene scene = <rootNode>.getScene();
     }
     
     
