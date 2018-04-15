7b8a9dad9a121a02d6917d068d291e9c54767aae
Daniel Klatt <dk0034@uah.edu>
2018-04-15 16:23:24 -0500

    Listeners & ID Searches to Composite Schedule
    Added ways to add person and shift listeners to schedule, along with
    methods to notify listeners. Also completed basic delegate functionality

--- src/edu/uah/cs321/team2/scheduleplanner/model/CompositeSchedule.java
+++ src/edu/uah/cs321/team2/scheduleplanner/model/CompositeSchedule.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import edu.uah.cs321.team2.scheduleplanner.PersonDelegate;
 import edu.uah.cs321.team2.scheduleplanner.ShiftDelegate;
+import edu.uah.cs321.team2.scheduleplanner.PersonListener;
+import edu.uah.cs321.team2.scheduleplanner.ShiftListener;
 
 /**
  * The model that contains the collections of data that make up
@@ -17,6 +19,8 @@ public class CompositeSchedule implements Serializable, PersonDelegate, ShiftDel
     public CompositeSchedule() {
         this.people = new ArrayList<>();
         this.shifts = new ArrayList<>();
+        this.personListeners = new ArrayList<>();
+        this.shiftListeners = new ArrayList<>();
     }
     
     /**
@@ -44,7 +48,7 @@ public class CompositeSchedule implements Serializable, PersonDelegate, ShiftDel
      */
     public void addPersonToPeople(Person newPerson) {
         this.people.add(newPerson);
-        // send out a notification?
+        this.notifyPersonListeners();
     }
     
     /**
@@ -54,41 +58,91 @@ public class CompositeSchedule implements Serializable, PersonDelegate, ShiftDel
      */
     public void addShiftToShifts(Shift newShift) {
         this.shifts.add(newShift);
-        // send out a notification?
+        this.notifyShiftListeners();
+    }
+    
+    /**
+     * Adds a person listener to the collection of listeners to be notified when person changes occur
+     * 
+     * @param newListener The new listener to be notified when changes to persons occur
+     */
+    public void addListenerToPersonListeners(PersonListener newListener) {
+        this.personListeners.add(newListener);
+    }
+    
+    public void addListenerToShiftListeners(ShiftListener newListener) {
+        this.shiftListeners.add(newListener);
     }
     
     // Section for PersonDelegate methods
     @Override
     public void addPerson(Person newPerson) {
         this.addPersonToPeople(newPerson);
+        this.notifyPersonListeners();
     }
     
     @Override
     public void editPerson(Person updatedPerson) {
         // Find Person by ID
+        int index = indexOfPersonByID(updatedPerson.getIdentifier());
         // Replace Person in people
+        if (index != UNKNOWN_INDEX) {
+            this.people.set(index, updatedPerson);
+            this.notifyPersonListeners();
+        }
     }
     
     @Override
     public void deletePerson(Person person) {
         // Find Person by ID
+        int index = indexOfPersonByID(person.getIdentifier());
         // Remove Person from people
+        if (index != UNKNOWN_INDEX) {
+            this.people.remove(index);
+            this.notifyPersonListeners();
+        }
     }
     
     // Section for ShiftDelegate methods
     @Override
     public void updateShift(Shift updatedShift) {
         // FInd Shift by ID
+        int index = indexOfShiftByID(updatedShift.getIdentifier());
         // Replace Shift in shifts
+        if (index != UNKNOWN_INDEX) {
+            this.shifts.set(index, updatedShift);
+            this.notifyShiftListeners();
+        }
     }
     
-    public int indexOfPersonByID(long searchID) {
+    private int indexOfPersonByID(long searchID) {
         for(Person person : this.people) {
             if (person.getIdentifier() == searchID) {
                 return this.people.indexOf(person);
             }
         }
-        return -1;
+        return UNKNOWN_INDEX;
+    }
+    
+    private int indexOfShiftByID(long searchID) {
+        for(Shift shift : this.shifts) {
+            if(shift.getIdentifier() == searchID) {
+                return this.shifts.indexOf(shift);
+            }
+        }
+        return UNKNOWN_INDEX;
+    }
+    
+    private void notifyPersonListeners() {
+        for(PersonListener listener : this.personListeners) {
+            listener.peopleUpdated();
+        }
+    }
+    
+    private void notifyShiftListeners() {
+        for(ShiftListener listener : this.shiftListeners) {
+            listener.shiftsUpdated();
+        }
     }
     
     @Override
@@ -97,7 +151,10 @@ public class CompositeSchedule implements Serializable, PersonDelegate, ShiftDel
                 + ", scheduleShifts=" + this.shifts + "]";
     }
     
+    private static final int UNKNOWN_INDEX = -1;
     private static final long serialVersionUID = 1L;
     private ArrayList<Person> people;
     private ArrayList<Shift> shifts;
+    private ArrayList<PersonListener> personListeners;
+    private ArrayList<ShiftListener> shiftListeners;
 }
